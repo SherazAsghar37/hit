e4fb548040477219c1b569f53ea1138f82fd7f83
sheraz2045 <sheraz@iotsands.com>
2025-01-17 01:06:11 +0500

    first commit

---
+++ lib/core/common/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class Theme {
+  //light theme color swatch
+  static const Color lightThemePrimaryTint = Color(0xFF9E9CDC);
+  static const Color lightThemePrimaryColor = Color(0xFF524EB7);
+  static const Color lightThemeSecondaryColor = Color(0xFFF76631);
+  static const Color lightThemePrimaryTextColor = Color(0xFF282344);
+  static const Color lightThemeSecondaryTextColor = Color(0xFF9491A1);
+  static const Color lightThemePinkColor = Color(0xFFF08E98);
+  static const Color lightThemeWhiteColor = Color(0xFFFFFFFF);
+  static const Color lightThemeTintStockColor = Color(0xFFF6F6F9);
+  static const Color lightThemeYellowColor = Color(0xFFFEC613);
+  static const Color lightThemeStockColor = Color(0xFFE4E4E9);
+  //dark theme color swatch
+  static const Color darkThemeDarkSharpColor = Color(0xFF191821);
+  static const Color darkThemeBGColor = Color(0xFF0E0D11);
+  static const Color darkThemeDarkNavBarColor = Color(0xFF201F27);
+}
